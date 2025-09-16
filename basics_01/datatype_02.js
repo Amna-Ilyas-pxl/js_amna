81124525bd2e91f1280608b9b8e7d8899362d612
Amna Ilyas <120961095+Silly-Viper@users.noreply.github.com>
2025-09-16 20:14:11 +0000

    datatypes

---
+++ basics_01/datatype_02.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all js code as newer version, once used we can not undo it after some code of this file
+// alert(3+3) //we are using node js not browser // it will give anser in alert box on browser but error here
+console.log(3
+    +3); console.log("amna") //readability matters. do not do this
+
+    //datatypes:
+    let name= "Amna"
+    let age= 18
+    let isLoggedIn= false    //bolean
+    let state; //undefined value // not null
+
+
+ //premitive datatypes   
+    //number => 2 to power 53
+    // bigInt => much larger values like stocks
+    //string => "" //strings are preferred
+    //bolean => true/false
+    //null => standalone value //representation of empty value
+    //undefined => value is not defined yet to any datatype
+    //symbol => to fined uniqueness, mostly used in react
+
+
+
+
+console.log(typeof "amna"); //tells datatype
+console.log(typeof age); //tells datatype
+console.log(typeof null); //tells datatype //object
+console.log(typeof undefined); //tells datatype
