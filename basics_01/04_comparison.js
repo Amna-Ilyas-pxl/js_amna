a4eb31a46d7d548b2d8215c6d8abf5abbe7db2be
Amna Ilyas <120961095+Silly-Viper@users.noreply.github.com>
2025-09-18 20:36:56 +0000

    comparison functions

--- basics_01/04_comparison.js
+++ basics_01/04_comparison.js
@@ -9,6 +9,7 @@
 // console.log("2" > 1);
 // console.log("02" > 1); // js automatically converts string into number
 
+
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);  // value conversion
