fe50b65745a6116c5ec9150f1cb852507bdbfb4f
Amna Ilyas <120961095+Amna-Ilyas-pxl@users.noreply.github.com>
2025-09-21 18:53:23 +0000

    math and number

--- basics_01/06_nums_and_maths.js
+++ basics_01/06_nums_and_maths.js
@@ -1,45 +1,45 @@
 /////////// numbers //////////
 
-// const score = 400
-// console.log(score);
+const score = 400
+console.log(score);
 
-// const balance = new Number(100)
-// console.log(balance); // run console.log(balance), it specifies the following value is number
-
-
-// //  run these both on chrome console, the next will give functional properties like toString()
-// // {note: if you don't remember any function you can always use console to know more}
+const balance = new Number(100)
+console.log(balance); // run console.log(balance), it specifies the following value is number
 
 
-// console.log(balance.toString().length); //it will give the lentgh of charcters as now its is string not number
-
-// console.log(balance.toFixed(2)); //gives 2  values after decimal
-
-
-// const otherNumber = 23.899 //later give value like 11234.677 to see exponential value, so use precision carefully
-
-// console.log(otherNumber.toPrecision(3));  //important for interview
-
-// const hundreds = 1000000
-// console.log(hundreds.toLocaleString()); //standard us value with commas
-// console.log(hundreds.toLocaleString('en-IN')) //indian standard
+//  run these both on chrome console, the next will give functional properties like toString()
+// {note: if you don't remember any function you can always use console to know more}
 
 
-// // for dsa side check console chrome and enter number to see multiple functions for number ,max/min values(note:max is not bigInt) and others
+console.log(balance.toString().length); //it will give the lentgh of charcters as now its is string not number
+
+console.log(balance.toFixed(2)); //gives 2  values after decimal
 
 
-/////////////// Maths ////////////////
+const otherNumber = 23.899 //later give value like 11234.677 to see exponential value, so use precision carefully
 
-// important in javascript
+console.log(otherNumber.toPrecision(3));  //important for interview
 
-// console.log(Math);
-// console.log(Math.abs(-4));  // absolute value, positive value remains positive while negative value converts to positive value
-// console.log(Math.round(4.3));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));  // if little bit above value then it will round off completely
-// console.log(Math.floor(4.6)); //take lowest value, doesn't rounds off
-// console.log(Math.min(4, 6, 3, 7)); //finds minimum value from array
-// console.log(Math.max(4, 6, 3, 7)); //finds maximum value from array
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //standard us value with commas
+console.log(hundreds.toLocaleString('en-IN')) //indian standard
+
+
+// for dsa side check console chrome and enter number to see multiple functions for number ,max/min values(note:max is not bigInt) and others
+
+
+///////////// Maths ////////////////
+
+important in javascript
+
+console.log(Math);
+console.log(Math.abs(-4));  // absolute value, positive value remains positive while negative value converts to positive value
+console.log(Math.round(4.3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));  // if little bit above value then it will round off completely
+console.log(Math.floor(4.6)); //take lowest value, doesn't rounds off
+console.log(Math.min(4, 6, 3, 7)); //finds minimum value from array
+console.log(Math.max(4, 6, 3, 7)); //finds maximum value from array
  
 //imp math library: random
 console.log(Math.random());  //value of math.random will always be between 0 and 1 , and everytime you run it will give off random values
@@ -48,3 +48,15 @@ console.log(Math.random()*10); // this will still give 0 as well, so to avoid th
 
 console.log(Math.random()*10 + 1); //minimum value is 1 now, this is mostly used for problems like rolling a dice in which there is no zeroes but values between 1 to 6(this is just one example)
 
+console.log(Math.floor(Math.random()*10 + 1));  //also be written like this
+
+
+//most imp now:
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random * (max - min + 1))+ min);
+
+// math.floor will print numbers without decimal(for single digit)
+// +1 will avoid zero
+// +min will add minimum value to formula which is assigned as 10 above.. so the values will be above or equal to 10
