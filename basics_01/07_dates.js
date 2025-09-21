95350b1a0d7dbe81a633cdb94c07142bc072bd21
Amna Ilyas <120961095+Amna-Ilyas-pxl@users.noreply.github.com>
2025-09-21 19:47:46 +0000

    Dates

--- basics_01/07_dates.js
+++ basics_01/07_dates.js
@@ -1,14 +1,14 @@
 //dates
 
-// let myDate = new Date() // object and it's instance
-// // console.log(myDate); // run to check, it is not readable
+let myDate = new Date() // object and it's instance
+// console.log(myDate); // run to check, it is not readable
  
-// //better:
+//better:
 
-// console.log(myDate.toString()); //try every option shown in list to show many better options
+console.log(myDate.toString()); //try every option shown in list to show many better options
 
-// //check it's type for interview (although it's converted to string it is object) 
-// console.log(typeof myDate);
+//check it's type for interview (although it's converted to string it is object) 
+console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2025, 0 , 25) //in array count starts from zero for january
 // console.log(myCreatedDate.toDateString());
@@ -17,16 +17,16 @@
 // console.log(myCreatedDate.toLocaleString());
 
 
-// let myCreatedDate = new Date("2025-01-04") //count starts from 1 in yyyy mm dd
-// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date("2025-01-04") //count starts from 1 in yyyy mm dd
+console.log(myCreatedDate.toLocaleString());
 
 let myTampStamp = Date.now()
-// console.log(myTampStamp); //milisecond value from the date we entered till today
+console.log(myTampStamp); //milisecond value from the date we entered till today
 
-// console.log(myCreatedDate.getTime());
+console.log(myCreatedDate.getTime());
 
 
-// console.log(Math.floor(Date.now()/1000));  //give in  miliseconds then divide by thousand and add math floor to avoid decimal(we can get small value this way)
+console.log(Math.floor(Date.now()/1000));  //give in  miliseconds then divide by thousand and add math floor to avoid decimal(we can get small value this way)
 
 
 let newDate = new Date()
